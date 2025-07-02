7e4f7a7e791195ae210b867d78c8eb546638943f
smaiaabel <smaiaabel@gmail.com>
2025-07-02 14:29:09 -0300

    Update commit_automatico.bat

--- Documents/Python studys/commit_automatico.bat
+++ Documents/Python studys/commit_automatico.bat
@@ -1,14 +1,8 @@
 @echo off
-REM Script para commit automático no Git
 
-REM Adiciona todos os arquivos alterados
 git add .
-
-REM Cria um commit com data e hora atuais
 set DATAHORA=%date% %time%
 git commit -m "Commit automático em %DATAHORA%"
-
-REM Envia para o repositório remoto
 git push
 
 pause
